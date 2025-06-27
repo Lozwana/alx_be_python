5031e9d64538c9c0015fd7a1deeb72a3908773cd
Lozwana <mabasohappyness@gmail.com>
2025-06-27 19:46:35 +0200

    Calculator script

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+oparator = input("Choose the operation (+, -, *, /): ").strip()
+
+match oparator:
+    case "+":
+        sum = num1 + num2
+        print(f"The result is {sum}.")
+        
+    case "-":
+        subtraction = num1 - num2
+        print(f"The result is {subtraction}.")
+        
+    case "*":
+        multiplication = num1 * num2
+        print(f"The result is {multiplication}.")
+        
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            divide = num1 / num2
+            print(f"The result is {divide}.")
+        
+    case _:
+        print("Invalid oparator. Please use +, -, *, or /.")
