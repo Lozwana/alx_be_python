5791611149ec1d594da09c6bb45328f9ea3e2bc2
Lozwana <mabasohappyness@gmail.com>
2025-06-27 19:54:26 +0200

    spelling fix

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 oparator = input("Choose the operation (+, -, *, /): ").strip()
 
-match oparations:
+match operations:
     case "+":
         sum = num1 + num2
         print(f"The result is {sum}.")
